92a03cb81e94fb1cd243c800cfaac3f242615ca1
Admin <admin@SEUNMEME.local>
2018-12-29 02:10:55 -0800

    add code for user input

--- 01_HELLO_WORLD/program.py
+++ 01_HELLO_WORLD/program.py
@@ -1,3 +1,9 @@
 print('-----------------------------------')
 print('            HELLO WORLD ')
 print('-----------------------------------')
+print()
+
+user_input = input('What is your name? ')
+greeting = 'Nice to meet you ' + user_input
+
+print(greeting)
