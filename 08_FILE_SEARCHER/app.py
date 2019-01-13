d7508bf6ced7e97c7d09d681efab3bd86f8a6108
Seun Odewale <seunodewale.zone5@gmail.com>
2019-01-13 15:15:36 +0100

    add logic for seaching files for specific text.

---
+++ 08_FILE_SEARCHER/app.py
@@ -0,0 +1,71 @@
+import os
+
+def main():
+    print_header()
+    
+    folder = get_folder_from_user()
+
+    if not folder: 
+        print("Sorry we can't search that location")
+        return
+    
+    text = get_file_text_from_user()
+
+    if not text: 
+        print("Sorry we can't search for nothing")
+        return    
+
+    matches = search_folders(folder, text)
+    for m in matches:
+        print(m)
+
+def print_header():
+    print('---------------------------------')
+    print('       FILE SEARCH APP')
+    print('---------------------------------')
+    print()
+
+def get_folder_from_user():
+    folder = input('What folder do you want to search? ')
+    if not folder or not folder.strip():
+        return None
+
+    if not os.path.isdir(folder):
+        return None
+
+    return os.path.abspath(folder)
+
+def get_file_text_from_user():
+    text = input('What are you searching for [ single phrases only ]? ')
+    return text.lower()
+
+def search_folders(folder, text):
+    # print('Would search {} for {}'.format(folder, text))
+    items = os.listdir(folder)
+
+    for item in items:
+        all_matches = []
+
+        full_item = os.path.join(folder, item)
+        if os.path.isdir(full_item):
+            continue
+
+        matches = search_file(full_item, text)
+        all_matches.extend(matches)
+
+        return all_matches
+
+
+def search_file(filename, search_text):
+    matches = []
+    with open(filename, 'r', encoding='utf-8') as fin:
+        for line in fin:
+            if line.lower().find(search_text) >= 0:
+                matches.append(line)
+
+        return matches
+
+
+
+if __name__ == '__main__':
+    main()
